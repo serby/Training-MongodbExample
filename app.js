79cd443f0e57b6de4ae21ad066a4c7f320eb8ada
Paul Serby <paul.serby@clock.co.uk>
2011-11-03 23:14:55 +0000

    Now with async

--- app.js
+++ app.js
@@ -1,21 +1,40 @@
 var
 	mongodb = require('mongodb'),
-	client = new mongodb.Db('ContactBook', new mongodb.Server('127.0.0.1', 27017, {}));
-
-function onCollection(err, collection) {
-
-	function addContact(contact, callback) {
-		collection.insert(contact, function(error, items) {
-			if (callback) {
-				callback(error, items[0]);
-			}
-		});
-	}
-
-	addContact({ name: 'Paul Serby', emailAddress: 'paul.serby@clock.co.uk'});
-	addContact({ name: 'Syd Nadim', emailAddress: 'syd.nadim@clock.co.uk'});
-}
+	client = new mongodb.Db('ContactBook', new mongodb.Server('127.0.0.1', 27017, {})),
+	async = require('async');
 
 client.open(function(error, client) {
+
+	function onCollection(error, collection) {
+
+		function addContact(contact, callback) {
+			collection.insert(contact, function(error, items) {
+				if (callback) {
+					callback(error, items[0]);
+				}
+			});
+		}
+
+		async.forEach([
+
+			{ name: 'Paul Serby', emailAddress: 'paul.serby@clock.co.uk'},
+			{ name: 'Syd Nadim', emailAddress: 'syd.nadim@clock.co.uk'}
+
+		], addContact, function(error) {
+			listAll(function() {
+				client.close();
+			});
+		});
+
+		function listAll(callback) {
+			collection.find({}).toArray(function(error, contacts) {
+				contacts.forEach(function(contact) {
+					console.log(contact);
+				});
+				callback();
+			});
+		}
+	}
+
 	client.collection('contact', onCollection);
 });
